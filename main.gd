2acc6793a484eb86b972361a19b95282d47d53f0
Alvaro <e.alvarolopez03@go.ugr.es>
2025-05-19 20:19:12 +0200

    Correcciones, uso de viewports para generar texturas para trazado de rayos

--- main.gd
+++ main.gd
@@ -2,15 +2,30 @@ extends Node3D
 
 @onready var cam_orbital := $OrbitalCamera/Camera3D
 @onready var cam_fp := $FirstPerson/Camera3D
+@onready var cam_viewport1 := $SubViewport/Camera3D
+@onready var viewport1 := $SubViewport
+@onready var mesh := $Box_delanteras
 
 func _ready():
 	cam_fp.current = false
 	cam_orbital.current = true
+	cam_viewport1.global_transform = cam_orbital.global_transform
+
+func _process(_delta):
+	if viewport1.get_texture():
+		mesh.get_active_material(0).set_shader_parameter("viewport_texture", viewport1.get_texture())
+	cam_viewport1.global_transform = cam_orbital.global_transform
 
 func _input(event):
 	if event.is_action_pressed("toggle_camera"):
-		cam_fp.current = !cam_fp.current
-		cam_orbital.current = !cam_fp.current
+		var use_fp = !cam_fp.current
+		cam_fp.current = use_fp
+		cam_orbital.current = !use_fp
+		#cam_fp.current = !cam_fp.current
+		#cam_orbital.current = !cam_fp.current
+
+		#var active_cam = cam_fp if use_fp else cam_orbital
+		cam_viewport1.global_transform = cam_orbital.global_transform
 		
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
